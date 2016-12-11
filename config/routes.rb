519f54877a0e0ea7e505e9619f0056fad36bd92c
alexv7 <alexvarjabedian7@gmail.com>
2016-12-11 16:37:15 -0500

    this is how you add the custom name attribute to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :listings
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root 'welcome#index'
   resources :posts
   # resources :users
